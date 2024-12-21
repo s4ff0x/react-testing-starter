4fcde96f0356c1c60280a41d7bfbd46b0d2d8a69
antonm <mendelson.inbox@gmail.com>
2024-12-21 22:30:40 +0200

    feat: 28-29 lessons

--- src/tests/setup.ts
+++ src/tests/setup.ts
@@ -1,12 +1,12 @@
 import "@testing-library/jest-dom/vitest";
+import { server } from "./mocks/server";
+import ResizeObserver from "resize-observer-polyfill";
 
-const ResizeObserverMock = vi.fn(() => ({
-  observe: vi.fn(),
-  unobserve: vi.fn(),
-  disconnect: vi.fn(),
-}));
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
 
-vi.stubGlobal("ResizeObserver", ResizeObserverMock);
+global.ResizeObserver = ResizeObserver;
 
 window.HTMLElement.prototype.scrollIntoView = vi.fn();
 window.HTMLElement.prototype.hasPointerCapture = vi.fn();
